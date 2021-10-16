d4875584f42a27a496a8f7285cd1e97a9a434fd5
vivek vardhan . M <84318895+vikkyvivek2002@users.noreply.github.com>
2021-10-16 10:16:28 +0530

    Add files via upload

---
+++ Exceptions.py
@@ -0,0 +1,13 @@
+x = "hell"
+try:
+    y = int(x)
+except:
+    y = 1
+print('first result ',y)
+x= '123'
+try:
+    y = int(x)
+except:
+    y =1
+print('second result ',y)
+print('completed')             
